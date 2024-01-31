77b2e8f81f0e130b57a63a0c3f1cee9f3f87697e
nshcherbakova <npoplevina@yandex.ru>
2024-01-31 12:27:52 -0800

    [ImageServer:Docker] added docker compose config

--- ImageServer/docker/run_docker.sh
+++ ImageServer/docker/run_docker.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-DEST_SERVER="192.168.86.176:8081"
+DEST_SERVER="192.168.126.107:8081"
 DOCKER_PORT="8081"
 
 echo "Build docker"
 
 cd ../
-docker build -f Dockerfile.txt . -t image-server
+docker build -f Dockerfile . -t image-server
 
 echo "Run ImageServer in docker in the address "$DEST_SERVER
 
